b34ec867c70705cee2e9b806147622cb9c6a2e16
minalan79 <94749642+minalan79@users.noreply.github.com>
2023-03-29 23:42:27 +0530

    4 files modified, 1 added, 1 deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,117 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import logo from "./public/logo.svg";
+import iconDollar from "./public/icon-dollar.svg";
+import iconPerson from "./public/icon-person.svg";
 
-function App() {
-  const [count, setCount] = useState(0);
+export default function App() {
+  const [tipPercentage, setTipPercentage] = useState(0);
+  const [customTip, setCustomTip] = useState(false);
+
+  const changeTipPercentage = (tipActive) => {
+    setTipPercentage(tipActive);
+  };
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <img src={logo} width="100px" alt="" />
+      <div className="tip-calculator-wrapper">
+        <div className="tip-calculator-inputs">
+          <div className="calculator-input-wrapper">
+            <p className="calculator-body-text">Bill</p>
+            <div className="input-bar">
+              {/* <span className="dollar">$</span> */}
+              <img src={iconDollar} className="dollar" alt="" />
+              <input type="number" name="bill-amount" id="bill-amount" />
+            </div>
+          </div>
+          <div className="tip-percentage-wrapper">
+            <p className="calculator-body-text">Select Tip %</p>
+            <span
+              className="tip-percentage"
+              onClick={() => changeTipPercentage(5)}
+              style={
+                tipPercentage === 5
+                  ? { backgroundColor: "var(--clr-strong-cyan)" }
+                  : {}
+              }
+            >
+              5%
+            </span>
+            <span
+              className="tip-percentage"
+              onClick={() => changeTipPercentage(10)}
+              style={
+                tipPercentage === 10
+                  ? { backgroundColor: "var(--clr-strong-cyan)" }
+                  : {}
+              }
+            >
+              10%
+            </span>
+            <span
+              className="tip-percentage"
+              onClick={() => changeTipPercentage(15)}
+              style={
+                tipPercentage === 15
+                  ? { backgroundColor: "var(--clr-strong-cyan)" }
+                  : {}
+              }
+            >
+              15%
+            </span>
+            <span
+              className="tip-percentage"
+              onClick={() => changeTipPercentage(25)}
+              style={
+                tipPercentage === 25
+                  ? { backgroundColor: "var(--clr-strong-cyan)" }
+                  : {}
+              }
+            >
+              25%
+            </span>
+            <span
+              className="tip-percentage"
+              onClick={() => changeTipPercentage(50)}
+              style={
+                tipPercentage === 50
+                  ? { backgroundColor: "var(--clr-strong-cyan)" }
+                  : {}
+              }
+            >
+              50%
+            </span>
+            {!customTip ? (
+              <span
+                className="tip-percentage tip-percentage-custom"
+                onClick={() => setCustomTip(true)}
+              >
+                Custom
+              </span>
+            ) : (
+              <div className="input-bar">
+                <input
+                  type="number"
+                  name="custom-tip"
+                  id="custom-tip"
+                  onChange={(e) => setCustomTip(e.target.value)}
+                />
+              </div>
+            )}
+          </div>
+          <div className="calculator-input-wrapper">
+            <p className="calculator-body-text">Number of People</p>
+            <div className="input-bar">
+              <img src={iconPerson} className="dollar" alt="" />
+              <input type="number" name="bill-amount" id="bill-amount" />
+            </div>
+          </div>
+        </div>
+        <div className="tip-calculator-results">
+          <p className="calculator-body-text">Tip Amount</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   );
 }
-
-export default App;
